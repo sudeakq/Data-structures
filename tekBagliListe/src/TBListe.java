6f9319e35cfdc4d735b2a5154828ba59bb516713
sudeakq <sudeakq@gmail.com>
2023-04-14 13:51:36 +0300

    m

--- tekBagliListe/src/TBListe.java
+++ tekBagliListe/src/TBListe.java
@@ -98,15 +98,30 @@ public class TBListe {
                        // null olarak dönmeli
 
     }
-    // odev:aranan degerden listede kac tane oldugunu geri donduren fonksiyonu kodla
-    // elemansay()
 
     // degeri,listede herhangi bir indekse ekleyen fonk
     public void arayaEkle(int deger, int indis) {
-        if (this.ilk == null) {
+        if (this.ilk == null || (indis == 1)) {
             this.basaEkle(deger);
             return;
         }
+        Dugum yeni = new Dugum(deger);
+        Dugum temp = this.ilk;
+        int sayac = 1;
+        while (temp.getSonraki() != null) {// eklenecek yerin bir önceki dugumune tempin konumlandırılması işlemi
+            if (sayac + 1 == indis) {
+                break;
+            }
+            sayac++;
+            temp = temp.getSonraki();
+        }
+        yeni.setSonraki(temp.getSonraki());// yesil baglanti
+        temp.setSonraki(yeni);// mavi baglanti
 
     }
+    // odev:aranan degerden listede kac tane oldugunu geri donduren fonksiyonu kodla
+    // elemansay()
+    // odev2: araya ekle fonk sona ekle ile ilgili kısmının kontrolu ve yazılmasi
+    // odev3: araya ekle fonk degerleri sayisal olarak sıralı ekleyecek sekilde
+    // yazılacak (indis parametresi olmayan)
 }
