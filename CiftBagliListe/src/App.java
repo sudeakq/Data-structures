af2b7cd2b2bb1c869265fd758d85b132e5fb23c7
sudeakq <sudeakq@gmail.com>
2023-04-27 11:32:27 +0300

    used linkedlist from java's

--- CiftBagliListe/src/App.java
+++ CiftBagliListe/src/App.java
@@ -1,14 +1,20 @@
+import java.util.LinkedList;
+
 public class App {
     public static void main(String[] args) throws Exception {
 
-        CBListe cbl = new CBListe();
+        // CBListe cbl = new CBListe();
 
-        cbl.listele();
-        cbl.basaEkle(20);
-        cbl.arayaEkle(10, 1);
-        cbl.sonaEkle(30);
+        // cbl.listele();
+        // cbl.basaEkle(20);
+        // cbl.arayaEkle(10, 1);
+        // cbl.sonaEkle(30);
 
-        cbl.listele();
+        // cbl.listele();
 
+        LinkedList<String> liste = new LinkedList<>();
+        liste.add("veri");
+        liste.addFirst("yapilari");
+        System.out.println("eleman sayısı:" + liste.size());
     }
 }
